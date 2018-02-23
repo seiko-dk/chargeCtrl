f1ff1fdb8b06e3d8e222fe3303fa31c842992f3c
Tajs Brandt <logreko@gmail.com>
2018-02-23 21:19:49 +0100

    Started on charger class

--- main.py
+++ main.py
@@ -5,17 +5,24 @@ from logging.config import fileConfig
 
 def send (data):
     print (data)
-    
+
 def getIO():
     res = modbusClient.ReadHoldingRegisters(24004, 2)
     logger.debug('IO: %s', res)
     return res
 
 
-    
-    
-send("hej")
+#send("hej")
 uart = '/dev/ttyUSB0'
+
+class ChargerIf(object):
+	def __init__(self, *params):
+		self._charging = 0
+		self._connected = 0
+		self._btnEnabled = 0
+
+	def update(self, inputs, outputs):
+		self._btnEnabled = 1
 
 modbusClient = ModbusClient.ModbusClient(uart)
 modbusClient.UnitIdentifier = 16                    #Set slave ID
@@ -23,15 +30,18 @@ modbusClient.Parity = ModbusClient.Parity.none
 modbusClient.Connect()
 modbusClient.ser.setDTR(False)                      #Enable autogating (half-duplex)
 
+charger = ChargerIf()
+charger.update(0,1)
+
 
 fileConfig('logging_config.ini')
 logger = logging.getLogger()
 
-i = 1
+i = 1 #24 * 60	#20 hours
 while (i > 0):
     getIO()
     i=i-1
-    sleep(1)
-    
+#    sleep(60)
+
 
 modbusClient.close()
